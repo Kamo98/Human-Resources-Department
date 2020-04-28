98a17add9ac9bda8aeeb80b1e234cd2eae529888
Kamo98 <avanesjan.k@gmail.com>
2020-04-28 10:26:23 +0700

    Finish

--- HRD/Form1.cs
+++ HRD/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,33 @@ namespace HRD
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			string login = "posgres";
+			string pass = "Ntcnbhjdfybt_01";
+			//string connectionString = "Server=hrd.cx7kyl76gv42.us-east-2.rds.amazonaws.com;Port=5432;User=" + login + ";Password=" + pass + ";Database=postgres;";
+
+			string connectionString = "Server = hrd.cx7kyl76gv42.us-east-2.rds.amazonaws.com; DataBase = postgres; Integrated Security = false; User Id = " + login + "; password = " + pass;
+
+			//Создание соединения с БД
+
+			NpgsqlConnection npgSqlConnection = new NpgsqlConnection(connectionString);
+
+			//Обработка исключения при создания исключения, тут выведется сообщение о неверном логине или пароле
+			try
+			{
+				npgSqlConnection.Open();
+
+				npgSqlConnection.Close();
+			}
+			catch (NpgsqlException)
+			{
+				npgSqlConnection = null;
+				MessageBox.Show("Подключение НЕ выполнено");
+			}
+
 		}
 	}
 }
